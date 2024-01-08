b102d3e8e05dd5f506ca00cb85cc1c403a90b6c0
Md. Mehedi Hasan <smmehedi06@gmail.com>
2024-01-08 23:22:50 +0600

    base entity added

--- DotNet8Template/App.Domain/Entities/Authentication/AppUser.cs
+++ DotNet8Template/App.Domain/Entities/Authentication/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace App.Domain.Entities.Authentication
 {
-    public class AppUser
+    public class AppUser : BaseEntity
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
--- DotNet8Template/App.Domain/Entities/Authentication/Role.cs
+++ DotNet8Template/App.Domain/Entities/Authentication/Role.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace App.Domain.Entities.Authentication
 {
-    public class Role
+    public class Role : BaseEntity
     {
     }
 }

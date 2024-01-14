d71f17da332761284bfb8ed3f1f7f5773b5b95b2
Md. Mehedi Hasan <smmehedi06@gmail.com>
2024-01-14 23:53:03 +0600

    infrastructure registration issue solved

--- DotNet8Template/App.Infrastructure/Helpers/InfrastructureRegistration.cs
+++ DotNet8Template/App.Infrastructure/Helpers/InfrastructureRegistration.cs
@@ -5,11 +5,11 @@ using Microsoft.Extensions.DependencyInjection;
 namespace App.Infrastructure.Helpers
 {
     public static class InfrastructureRegistration
-    {
+    {        
         public static IServiceCollection RegisterRepositories(this IServiceCollection services)
         {
-            //services.AddScoped<IUserTypeRepository, UserTypeRepository>();
-            //services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserTypeRepository, UserTypeRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
             return services;
         }
     }
--- DotNet8Template/App.Infrastructure/Repositories/User/UserRepository.cs
+++ DotNet8Template/App.Infrastructure/Repositories/User/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Infrastructure.RepositoryInterfaces.User;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace App.Infrastructure.Repositories.User
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
     }
 }

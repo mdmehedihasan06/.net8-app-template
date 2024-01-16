d85d8df54e1bde3cf1ce9d2673217ac5a025b469
Md. Mehedi Hasan <smmehedi06@gmail.com>
2024-01-17 00:57:56 +0600

    role repositories added

---
+++ DotNet8Template/App.Infrastructure/Repositories/Role/RoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using App.Infrastructure.RepositoryInterfaces.Role;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infrastructure.Repositories.Role
+{
+    public class RoleRepository : IRoleRepository
+    {
+    }
+}

cce111c3317488b1eeb223e4fe0d3426067b4073
Md. Mehedi Hasan <smmehedi06@gmail.com>
2024-01-17 00:53:39 +0600

    constants added

--- DotNet8Template/App.ClientModel/Constants/AppConstants.cs
+++ DotNet8Template/App.ClientModel/Constants/AppConstants.cs
@@ -8,6 +8,19 @@ namespace App.ClientModel.Constants
 {
     public class AppConstants
     {
-
+        public enum StatusId
+        {
+            Pending = 0,
+            Active = 1,
+            Inactive = 2
+        }
+        public enum ApprovalStatusId
+        {
+            Pending = 0,
+            PartiallyApproved = 1,
+            Approved = 2,
+            UnderReview = 3,
+            Rejected = 4
+        }
     }
 }

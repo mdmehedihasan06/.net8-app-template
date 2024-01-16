a659306858429f73c7e10308549da4ea2d7940f3
Md. Mehedi Hasan <smmehedi06@gmail.com>
2024-01-17 01:05:33 +0600

    infrastructure registration added

--- DotNet8Template/App.Infrastructure/Helpers/InfrastructureRegistration.cs
+++ DotNet8Template/App.Infrastructure/Helpers/InfrastructureRegistration.cs
@@ -1,4 +1,8 @@
-﻿using App.Infrastructure.Repositories.User;
+﻿using App.Infrastructure.Repositories.Menu;
+using App.Infrastructure.Repositories.Role;
+using App.Infrastructure.Repositories.User;
+using App.Infrastructure.RepositoryInterfaces.Menu;
+using App.Infrastructure.RepositoryInterfaces.Role;
 using App.Infrastructure.RepositoryInterfaces.User;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,8 +12,17 @@ namespace App.Infrastructure.Helpers
     {        
         public static IServiceCollection RegisterRepositories(this IServiceCollection services)
         {
+            // User
             services.AddScoped<IUserTypeRepository, UserTypeRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
+            
+            // Role
+            services.AddScoped<IRoleRepository, RoleRepository>();
+            services.AddScoped<IRoleMenuPermissionRepository, RoleMenuPermissionRepository>();
+
+            // Menu
+            services.AddScoped<IMenuRepository, MenuRepository>();
+            services.AddScoped<IUserMenuPermissionRepository, UserMenuPermissionRepository>();
             return services;
         }
     }
